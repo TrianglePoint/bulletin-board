3e333d0ee037102074f505a805e61528539c01d7
TrianglePoint <rlarbgus10@gmail.com>
2019-06-21 14:17:43 +0900

    CRUD with mapper of mybatis

--- BulletinBoard/src/main/java/org/trianglepoint/post/mapper/PostMapper.java
+++ BulletinBoard/src/main/java/org/trianglepoint/post/mapper/PostMapper.java
@@ -1,5 +1,17 @@
 package org.trianglepoint.post.mapper;
 
+import java.util.List;
+
+import org.trianglepoint.post.domain.PostVO;
+
 public interface PostMapper {
+	public void insert(PostVO post);
 	
+	public PostVO get(Long pono);
+	
+	public List<PostVO> getList();
+	
+	public int update(PostVO post);
+	
+	public int delete(Long pono);
 }
